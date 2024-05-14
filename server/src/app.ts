250253493e72362ee935345ba834841c190f34fb
DiegoOno <diego.kazuo@azulsystems.com.br>
2024-05-14 13:13:58 -0300

    feat: setting up prisma db

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,9 @@
 import { startServer } from "./lib/lib";
 
 const server = startServer();
+
+server.get("/", () => {
+  return { message: "Hello World" };
+})
+
 server.listen({ port: parseInt(process?.env?.NODE_ENV_SERVER_PORT || "3000") });
